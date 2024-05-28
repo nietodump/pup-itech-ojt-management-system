11ecb524c340ba912591b6f6c3640a3efd81ee60
officialNietoA <nieto.alfns@gmail.com>
2024-05-28 12:34:45 +0800

    changes template

--- student/template.php
+++ student/template.php
@@ -49,7 +49,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  <title>OJT Monitoring Record System</title>
+  <title>ITOP - OJT Management System</title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Font Awesome Icons -->
